1caaf4b621c108bfec9f1b616df4ebf40a5626dd
Shien <f0928652413@gmail.com>
2022-04-12 14:26:53 +0800

    Initial Commit

---
+++ putTheEgg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  putTheEgg
+//
+//  Created by 方仕賢 on 2022/4/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
